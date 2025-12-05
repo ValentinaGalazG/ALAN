2305f8b8e704b1d2785d31a4bf617f4fce60590b
ValentinaGalazG <valentinaantonellagalaz@gmail.com>
2025-12-05 00:38:16 -0300

    Script para cargar datos de Crecimiento

--- Scripts/01_Carga-Datos-Consumo-Alimento-Girella-Laevifrons_ALAN_2023.R
+++ Scripts/01_Carga-Datos-Consumo-Alimento-Girella-Laevifrons_ALAN_2023.R
@@ -1,10 +1,10 @@
 #DETALLE DEL SCRIPT (LEER)
 
 # El presente script nos permitirá descargar los datos correspondientes al ensayo realizado durante el mes de Junio del año 2023, sobre los organismos juveniles de Girella laevifrons bajo las condiciones experimentales descritas y detalladas en el archivo "README.md"
-# En la sección #1 debemos descargar e instalar el paquete de R que nos permitirá leer archivos ".csv"
+# En la sección #1 se debe descargar e instalar el paquete de R que nos permitirá leer archivos ".csv"
 # En la sección #2 se debe cargar la librería para leer el paquete.
 # Los datos deben ser cargados desde mi carpeta "Data" dentro del repositorio "ALAN". 
-# Cada script de carga de datos corresponde a sólo 1 dataset. El presente corresponde a la lectura de datos de Crecimiento.
+# Cada script de carga de datos corresponde a sólo 1 dataset. El presente corresponde a la carga de datos de "Crecimiento".
 
 # 1. Descarga de paquetes -------------------------------------
 install.packages("readr")
