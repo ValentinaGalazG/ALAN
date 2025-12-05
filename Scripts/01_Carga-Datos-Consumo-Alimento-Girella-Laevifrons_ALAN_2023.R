217bab71c7517193b7958a1b4905f24b2d9df99b
ValentinaGalazG <valentinaantonellagalaz@gmail.com>
2025-12-05 00:50:01 -0300

    commit final

--- Scripts/01_Carga-Datos-Consumo-Alimento-Girella-Laevifrons_ALAN_2023.R
+++ Scripts/01_Carga-Datos-Consumo-Alimento-Girella-Laevifrons_ALAN_2023.R
@@ -2,7 +2,7 @@
 
 # El presente script nos permitirá descargar los datos correspondientes al ensayo realizado durante el mes de Junio del año 2023, sobre los organismos juveniles de Girella laevifrons bajo las condiciones experimentales descritas y detalladas en el archivo "README.md"
 # Los datos deben ser cargados desde mi carpeta "Data" dentro del repositorio "ALAN". 
-# Cada script de carga de datos corresponde a sólo 1 dataset. El presente corresponde a la carga de datos de "Crecimiento".
+# Cada script de carga de datos corresponde a sólo 1 dataset (. El presente corresponde a la carga de datos de "Crecimiento".
 
 # 1. Descarga de paquetes -------------------------------------
 install.packages("readr") #Paquete "readr" permite la importación de datos desde archivos de texto ".csv" a Dataframes en R.
@@ -16,4 +16,4 @@ crecimiento <- read_csv2(datos_crecimiento) #Se utiliza read_csv2 ya que este ar
 
 # 4. Verificación de carga de datos
 str(crecimiento) # La función str() permite ver el número de filas, columnas y el nombre de las mismas.
-head(crecimiento) # La función head() se utiliza para visualizar las primeras filas del dataframe y así asegurarnos de tanto los datos como las variables estén identificables, separadas y en orden
+head(crecimiento) # La función head() se utiliza para visualizar las primeras filas del dataframe y así asegurarnos de que tanto los datos como las variables estén identificables, separadas y en orden
